a049865251a501ee83738ae1834f9d66fe619a74
Xemn <spysschaert.pro@gmail.com>
2022-08-01 18:13:00 +0200

    Create the function signup in order to create and save a user in our mongo database

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,3 +4,22 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 // Import de notre module jsonwebtoken :
 const jwt = require("jsonwebtoken");
+
+// Creation du controller concernant l'inscription d'un utilisateur :
+exports.signup = (req, res, next) => {
+  // On commence en premier lieu à hasher le mot de passe :
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      // Après obtention de ce hash nous pouvons créer l'utilisateur :
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur crée !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
