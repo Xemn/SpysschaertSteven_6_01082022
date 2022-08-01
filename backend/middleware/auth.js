3d6becc278396b4300e3abb1fa6cb0e06f7d5c11
Xemn <spysschaert.pro@gmail.com>
2022-08-01 19:16:09 +0200

    Create a function who will decode and verify our token in order to allow user to do some actions

---
+++ backend/middleware/auth.js
@@ -0,0 +1,24 @@
+// On importe notre module jsonwebtoken :
+const jwt = require("jsonwebtoken");
+
+// Export de notre middlwarre :
+module.exports = (req, res, next) => {
+  /* Plusieurs erreurs pouvant se produire, nous mettons donc nos instructions
+  dans un bloc try...catch :  */
+  try {
+    // On récupère en premier lieu notre token de notre header:
+    const token = req.headers.authorization.split(" ")[1];
+    // On décode et vérifie la conformité de notre token :
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    // On en extrait notre userId, encodé dans le token :
+    const userId = decodedToken.userId;
+    /* On ajoute à notre requête un objet auth afin qu'il contienne l'userID
+    de note token : */
+    req.auth = { userId };
+    next();
+  } catch (error) {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
+};
