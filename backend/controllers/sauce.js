8b091760edf4b3371d37f4cd3ede0fa3e1f6199b
Xemn <spysschaert.pro@gmail.com>
2022-08-01 19:07:01 +0200

    Import our Sauce model, and the fs module; Create a createSauce function who will creeate and save a sauce in our mongo database

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,25 @@
+// Import de notre modèle Sauce :
+const Sauce = require("../models/Sauce");
+// Import du module fs :
+const fs = require("fs");
+/* Dans ce fichier on dépose la logique métier afin que notre fichier de routes,
+ne comporte lui que la logique de routing :  */
+
+// Logique métier pour la création d'une sauce :
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  // console.log(req.body.sauce);
+  // Création d'une nouvelle sauce :
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+  sauce
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Objet enregistré ! " });
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
