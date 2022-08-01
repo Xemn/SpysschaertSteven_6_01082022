d22a7f54614a87bb162ba9e4605ef95caa399db5
Xemn <spysschaert.pro@gmail.com>
2022-08-01 19:38:55 +0200

    Import our dotenv module, and replace the keyphrase by a variable

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,8 @@
 // On importe notre module jsonwebtoken :
 const jwt = require("jsonwebtoken");
+// On importe notre module dotenv :
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Export de notre middlwarre :
 module.exports = (req, res, next) => {
@@ -9,7 +12,7 @@ module.exports = (req, res, next) => {
     // On récupère en premier lieu notre token de notre header:
     const token = req.headers.authorization.split(" ")[1];
     // On décode et vérifie la conformité de notre token :
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, TOKEN_KEYPHRASE);
     // On en extrait notre userId, encodé dans le token :
     const userId = decodedToken.userId;
     /* On ajoute à notre requête un objet auth afin qu'il contienne l'userID
