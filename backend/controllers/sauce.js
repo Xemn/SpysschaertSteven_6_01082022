530abf8de4c96c1093c8d2944e8e1c868eb814a2
Xemn <spysschaert.pro@gmail.com>
2022-08-01 19:09:27 +0200

    Create a function who will allow the user who create the sauce to modify her and so update her in our mongo database

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -35,3 +35,33 @@ exports.getAllSauces = (req, res, next) => {
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
 };
+// Logique métier pour modifier une sauce :
+exports.updateOneSauce = (req, res, next) => {
+  // On vérifie si notre requête possède un fichier dans son corps :
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  /* On vérifie si l'utilisateur faisant la requête est aussi la 
+    personne qui a crée la sauce :  */
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    // Si c'est pas le cas :
+    if (sauce.userId != req.auth.userId) {
+      res.status(401).json({
+        message: "Vous n'êtes pas autorisé à faire cette action !",
+      });
+    } else {
+      // On modifie :
+      Sauce.updateOne(
+        { _id: req.params.id },
+        { ...sauceObject, _id: req.params.id }
+      )
+        .then(() => res.status(200).json({ message: "Sauce modifié !" }))
+        .catch((error) => res.status(400).json({ error }));
+    }
+  });
+};
