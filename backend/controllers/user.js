f7e9c5e17aef6b306045a04f6760d290341d97ea
Xemn <spysschaert.pro@gmail.com>
2022-08-01 18:39:02 +0200

    Create a login function in order for user tu be connected at our website

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,6 +4,9 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 // Import de notre module jsonwebtoken :
 const jwt = require("jsonwebtoken");
+//Import de notre module dotenv :
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Creation du controller concernant l'inscription d'un utilisateur :
 exports.signup = (req, res, next) => {
@@ -20,6 +23,46 @@ exports.signup = (req, res, next) => {
         .save()
         .then(() => res.status(201).json({ message: "Utilisateur crée !" }))
         .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+// Création du controller concernant la connexion d'un utilisateur :
+exports.login = (req, res, next) => {
+  /* On vérifie en premier lieu si l'addresse mail entrée lors de la 
+    tentavie de connexion existe dans notre base de données : */
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      // Si l'utilisateur n'existe pas :
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur introuvable" });
+      }
+      // Si il existe :
+      // Nous comparons le hash des deux mots de passes :
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          // Si le mot de passr est incorrecte :
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          // Si il est valide :
+          res.status(200).json({
+            // On crée un objet contenant un userId et un token :
+            userId: user._id,
+            token: jwt.sign(
+              // Données encodées (ou payload) :
+              { userId: user._id },
+              // Chaine secrète de developpement temporaire :
+              TOKEN_KEYPHRASE,
+              // Son délai d'expiration :
+              {
+                expiresIn: "24h",
+              }
+            ),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
