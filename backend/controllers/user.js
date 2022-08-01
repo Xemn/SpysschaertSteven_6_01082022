a337357a62c91dc36826e282eb97087fb7618694
Xemn <spysschaert.pro@gmail.com>
2022-08-01 18:11:54 +0200

    Import the module we need in order to create all user function

---
+++ backend/controllers/user.js
@@ -0,0 +1,6 @@
+// Import de notre modèle User :
+const User = require("../models/User");
+// Import du module bcrypt :
+const bcrypt = require("bcrypt");
+// Import de notre module jsonwebtoken :
+const jwt = require("jsonwebtoken");
