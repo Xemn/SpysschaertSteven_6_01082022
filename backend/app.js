f5d7e1f9b6e29c9b30c013ad2a6a0b12bebee636
Xemn <spysschaert.pro@gmail.com>
2022-08-01 17:53:31 +0200

    Our application file, with inside for the moment some modules import

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+// Import de notre module Express :
+const express = require("express");
+// Import de notre module Path :
+const path = require("path");
+// Import de notre module mongoose :
+const mongoose = require("mongoose");
